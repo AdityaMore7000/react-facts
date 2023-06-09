c36099fa506e36555dcb8701d096415a76c636f0
AdityaMore7000 <adi.more7000@gmail.com>
2023-06-09 16:24:56 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
  return (
-  <h1>Hello Vite</h1>
+  <h1 className=" w-fit bg-slate-400 absolute m-auto inset-0 text-3xl text-[#bd1717] font-sans p-2 h-fit rounded-full">Hello Vite</h1>
  )
 }
 
